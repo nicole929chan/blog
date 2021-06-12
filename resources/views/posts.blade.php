db632ac145098bfe07eba7fb6d72698668658791
Nicole <nicole929chan@gmail.com>
2021-06-12 17:28:56 +0800

    posts, users, categories

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,14 @@
+<x-layout>
+    @foreach ($posts as $post)
+    <article>
+        <h1>{!! $post->title !!}</h1>
+        <p>
+            By <a href="/authors/{{ $post->author->username }}">{{ $post->author->name  }}</a> in <a href="#">{{ $post->category->name }}</a>
+        </p>
+        <div>
+            {!! $post->body !!}
+        </div>
+    </article>
+    <hr>
+    @endforeach
+</x-layout>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Models\Post;
+use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +15,19 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('posts', function () {
+    // use "without" to turn off the eager load effect
+    return view('posts', [
+        'posts' => Post::latest()->get()
+    ]);
+});
+
+Route::get('/posts/{post}', function (Post $post) {
+    return view('post', ['post' => $post]);
+});
+
+Route::get('/authors/{author:username}', function (User $author) {
+    return view('posts', [
+        'posts' => $author->posts
+    ]);
 });
